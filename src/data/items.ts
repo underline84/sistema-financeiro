ffa15e0ff9350b57e9684518af02de5e81bae63d
underline84 <evanfatu@hotmail.com>
2022-10-03 21:35:11 -0300

    first commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import {Item} from '../types/Items';
+
+export const items: Item[] = [
+    {date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    {date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28},
+    {date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300},
+    {date: new Date(2021, 9, 18), category: 'salary', title: 'Salario ACME', value: 4500},
+]

a66a0d95a1d6e21be8a88c517f40739ac52b77e8
underline84 <evanfatu@hotmail.com>
2022-10-04 21:10:19 -0300

    04-10-2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useState, useEffect } from 'react';
 import * as C from './App.styles';
+import { Category } from './types/Category';
+import { Item } from './types/Items';
+import { categories } from './data/categories';
+import { items } from './data/items';
+import { filterListByMonth, getCurrentMonth } from './helpers/dataFilters';
+import { TableArea } from './components/TableArea'
 
 const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth));
+  },[list, currentMonth]);
+
   return (
     <C.Container>
       
@@ -10,6 +25,8 @@ const App = () => {
 
       <C.Body>
         
+      <TableArea/>
+
       </C.Body>
 
     </C.Container>
